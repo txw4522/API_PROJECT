658b99d44d8be30a62c4b40fb49a1deb1fe3c4ea
Tony Weng <txw4522@g.rit.edu>
2021-03-14 20:20:28 -0400

    Added bootstrap template

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -2,6 +2,7 @@ const fs = require('fs'); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
+const bootstrap = fs.readFileSync(`${__dirname}/../client/cover.css`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
@@ -12,6 +13,7 @@ const getIndex = (request, response) => {
 const getCSS = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/css' });
   response.write(css);
+  response.write(bootstrap);
   response.end();
 };
 
